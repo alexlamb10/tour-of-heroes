09d3659f416a4b8f162031a5f08c0324b9315095
Alex Lamb <alexlamb104@gmail.com>
2023-04-04 18:02:50 -0600

    Added search functionality

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,18 +24,19 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
-  }
-  save(): void {
-  if (this.hero) {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
-  }
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
